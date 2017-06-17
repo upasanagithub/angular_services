db79d419569dd3600c48305692697c003eaec3df
Upasana Mahanta <ups.win@gmail.com>
2017-06-17 19:20:55 +0530

    2nd commit

---
+++ app.js
@@ -0,0 +1,12 @@
+(
+  angular.module('myApp',[]).controller('myCont',callMyCont).service('mySvc',mySvc);
+
+  callMyCont.$inject('mySvc');
+  function callMyCont(mySvc){
+    callMySvc();
+  }
+
+  function mySvc(){
+    $scope.msg = "1st Service";
+  }
+)();
